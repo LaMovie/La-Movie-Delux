89d600fc7380e4d6ac2fffda9fccb6d3e6fca2e6
LaMovie <156336185+LaMovie@users.noreply.github.com>
2025-04-10 00:32:02 -0400

    Add files via upload

--- Buscador.js
+++ Buscador.js
@@ -127,6 +127,8 @@ h1 {
   <li><a href="https://drive.google.com/file/d/1eSDfIDWjKOqDaRX4U0eG53obfR3zSw-1/view?usp=drivesdk" class="Data">🍿Los Horrores De Caddo Lake</a></li>
   <li><a href="https://archive.org/download/strario-tv-y-deportes-v-1.0.2-estable/Strario%20-%20TV%20y%20Deportes%20-%20v1.0.2%20Estable.apk" class="Data">📺Strario Tv</a></li>
   <li><a href="https://drive.google.com/drive/folders/1KC_t3WcbLaF3INgIfZcW5I7uXZ7nAxmU" class="Data">🍿La Rueda Del Tiempo 3</a></li>
+  <li><a href="https://drive.google.com/file/d/1nrGqoPR0FhL5kRN7GXjsdCImWN5kx0bT/view?usp=drivesdk" class="Data">🍿Novocaine</a></li>
+  <li><a href="https://drive.google.com/file/d/1HLFGCKgev_X63xZHSBVIdaEGlg54k_6m/view?usp=drivesdk" class="Data">🍿Rescate Implacable</a></li>
   <li><a href="" class="Data">🍿</a></li>
   <li><a href="" class="Data">🍿</a></li>
   <li><a href="" class="Data">🍿</a></li>
