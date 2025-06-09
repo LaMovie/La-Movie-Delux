98300aeb2a39963aef6e1ec800737fbda24f2735
LaMovie <156336185+LaMovie@users.noreply.github.com>
2025-06-09 17:53:12 -0400

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,132 @@
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+// config param
+let countItem = items.length;
+let itemActive = 0;
+// event next click
+next.onclick = function(){
+    itemActive = itemActive + 1;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
+}
+//event prev click
+prev.onclick = function(){
+    itemActive = itemActive - 1;
+    if(itemActive < 0){
+        itemActive = countItem - 1;
+    }
+    showSlider();
+}
+// auto run slider
+let refreshInterval = setInterval(() => {
+    next.click();
+}, 5000)
+function showSlider(){
+    // remove item active old
+    let itemActiveOld = document.querySelector('.slider .list .item.active');
+    let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+    itemActiveOld.classList.remove('active');
+    thumbnailActiveOld.classList.remove('active');
+
+    // active new item
+    items[itemActive].classList.add('active');
+    thumbnails[itemActive].classList.add('active');
+
+    // clear auto time run slider
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(() => {
+        next.click();
+    }, 5000)
+}
+
+// click thumbnail
+thumbnails.forEach((thumbnail, index) => {
+    thumbnail.addEventListener('click', () => {
+        itemActive = index;
+        showSlider();
+    })
+})
+
+Music.addEventListener('click', function(){
+   Nota.style.display = "block";
+ Nota.style.animation = 'In .8s linear forwards';
+});
+      function Cerrar(){
+    Nota.style.animation = 'Fin .8s linear forwards';
+}
+
+         <!-- BUSCADOR -->
+
+   Search.addEventListener('click', function() {
+  if(Aux.style.display == 'block') {
+     Aux.style.display = 'none';
+     Menu.style.marginLeft = '0vh';
+   Logo.style.marginLeft = '0vh';     
+  } else {
+     Aux.style.display = 'block';
+     Menu.style.marginLeft = '50vh';
+   Logo.style.marginLeft = '-50vh';
+  }
+});     
+
+        <!-- LINKS IFRAME -->
+
+function Links(iframe) {
+    var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+    var links = iframeDocument.getElementsByTagName('a');
+
+    for (let i = 0; i < links.length; i++) {
+        links[i].setAttribute('target', '_blank');
+ }
+}
+        <!-- Musica -->
+
+    var videos = ['https://5ce9406b73c33.streamlock.net/RSD/ngrp:livestream_all/playlist.m3u8', 
+'https://lamovie.github.io/BASS-NATION-ONLINE/', 
+'https://lamovie.github.io/BASS-NATION-ONLINE/',  'https://stream.4fun.tv:8888/hls/4f.m3u8', 'https://streamtv.mediasector.es/hls/activatv/index.m3u8', 'https://is.gd/7CaWXu', 'https://lamovie.github.io/BASS-NATION-ONLINE/', 'https://lamovie.github.io/BASS-NATION-ONLINE/', 
+'https://lamovie.github.io/BASS-NATION-ONLINE/',];
+    var currentVideoIndex = 0;
+
+    function adelantarVideo() {
+      currentVideoIndex++;
+      if (currentVideoIndex >= videos.length) {
+        currentVideoIndex = 0;
+}
+      cambiarVideo();
+}
+    function retrocederVideo() {
+      currentVideoIndex--;
+      if (currentVideoIndex < 0) {
+        currentVideoIndex = videos.length - 1;
+}
+      cambiarVideo();
+}
+    function cambiarVideo() {
+      Video.src = videos[currentVideoIndex];
+      Video.load();
+      Video.play();
+}
+
+
+
+  var P = document.querySelectorAll('.P');
+  var T = document.querySelectorAll('.thumbnail');
+    
+ 
+  P.forEach(p => p.onclick = () => {
+  p.style.color = '#4f9';
+  T.forEach(img => img.style.opacity = '.2');
+  T.forEach(t => t.onclick = () => {
+  p.style.color = '#fff';
+  T.forEach(img => img.style.opacity = '1');
+ });
+}); 
+
+   
+
+
