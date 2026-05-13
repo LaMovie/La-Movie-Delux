cfab728bf239403434cc6342ec3be48cd1e0aa20
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-05-13 19:45:44 -0400

    Add files via upload

--- Script2.js
+++ Script2.js
@@ -55,9 +55,13 @@ document.addEventListener("keyup", e => {
      
         function Check() {
    var isMobile = /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+   
+   var URL = [ "https://latino.solo-latino.com/es/search?keyword=", "https://ver.123pelicula.com/es/search?keyword=", "https://flixlat.com/es/search?keyword=", "https://video.playspelis.com/es/search?keyword=", "https://ww20.321moviesfree.com/es/search?keyword="];
+ 
+ var STREAMs = Math.floor(Math.random() * URL.length);
     
     // Elegir dominio según dispositivo
-    var domain = isMobile ? 'https://latino.solo-latino.com/es/search?keyword=' : 'https://h5.swplayer.com/es/search?keyword=';
+    var domain = isMobile ? URL[STREAMs] : 'https://h5.swplayer.com/es/search?keyword=';
     
     window.location.href = domain + buscador.value;
        buscador.value = '';
