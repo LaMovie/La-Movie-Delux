3e6722a39823624878802930703395e066d52a23
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-06-24 00:07:47 -0400

    Add files via upload

--- ScriptLang.js
+++ ScriptLang.js
@@ -239,10 +239,10 @@ document.addEventListener("keyup", e => {
   
   // Manejo del Enter
   if (e.key === "Enter") {
-     buscador.value = "";
-           Check();
+          Check();
+     buscador.value = "";           
     }
-});
+
 
      
         function Check() {
@@ -256,6 +256,7 @@ document.addEventListener("keyup", e => {
        buscador.value = '';
     };
 
+    
        // SEARCH CLICK
     Search.onclick = () => {
     if (buscador.value !== '') {
@@ -263,7 +264,7 @@ document.addEventListener("keyup", e => {
       } 
    };
      
-
+});
          <!-- HTML -->
          
  var HTML =
@@ -309,7 +310,6 @@ h1 {
     <div>
         <input type="text" name="buscador" id="buscador" placeholder="Buscar...">
            <br/><br/>
-     <ul id="Lista"></ul>
     
            </div>
 </section>
@@ -319,25 +319,7 @@ h1 {
 
 
 
-fetch('https://ipapi.co/json/')
-  .then(response => response.json())
-  .then(data => {
-    const country = data.country_code; // Devuelve el código del país en dos letras (US, BR, CL, AR, etc.)
-    
-    const spanishSpeakingCountries = ['AR', 'BO', 'CL', 'CO', 'CR', 'CU', 'DO', 'EC', 'SV', 'ES', 'GT', 'HN', 'MX', 'NI', 'PA', 'PY', 'PE', 'PR', 'UY', 'VE'];
 
-    if (country === 'US') {
- window.location.href = '+LA-MOVIE-US+.html';
-    } else if (country === 'BR') {
- window.location.href = '+LA-MOVIE-BR+.html';
-    } else if (country === 'AR') {
-   alert('⚠️SE LE NEGÓ EL ACCESO A ARG 🇦🇷 A ESTE CONTENIDO, COMUNICARSE CON EL CREADOR SI QUIERE.⚠️');
-  window.location.href = 'https://sync-stream-chat-app.base44.app';
-    } 
-  })
-  .catch(error => {
-    console.error("Error al detectar la ubicación:");
-});    
  
 
 
