5a4672f51f6031e25c5888c0e70d9e25c325f566
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-07-01 03:59:32 -0400

    Add files via upload

--- Script2.js
+++ Script2.js
@@ -61,7 +61,7 @@ document.addEventListener("keyup", e => {
  var STREAMs = Math.floor(Math.random() * URL.length);
     
     // Elegir dominio según dispositivo
-    var domain = isMobile ? 'https://latino.solo-latino.com/es/search?keyword=' : 'https://h5.swplayer.com/es/search?keyword=';
+    var domain = isMobile ? 'https://h5.swplayer.com/es/search?keyword=' : 'https://h5.swplayer.com/es/search?keyword=';
     
     window.location.href = domain + buscador.value;
        buscador.value = '';
