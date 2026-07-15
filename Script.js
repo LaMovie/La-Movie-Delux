29404fc3953f83b1ff528b0972557fb2505a9407
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-07-15 14:52:14 -0400

    Add files via upload

--- Script.js
+++ Script.js
@@ -223,6 +223,14 @@ document.onclick = (event) => {
     }
 };
 
+   DownMusic.onclick = (e) => {
+       e.preventDefault();
+   var isMobile = /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    if (!isMobile) {
+        window.location.href = 'https://lamovie.github.io/MUSIC-CLOUD';
+    }
+  };
+
 
 
 var ENLACE = document.querySelectorAll('.Container a, .Gallery a');
