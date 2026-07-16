3c6176ffba2f9a98989269df10d4d95441557a20
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-07-16 19:55:08 -0400

    Add files via upload

--- Script2.js
+++ Script2.js
@@ -94,7 +94,7 @@ document.addEventListener("click", e => {
   var isMobile = /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);   
   
           // ?texto=
-  const urlDestino = `${'go:GOOGLE'}?texto=${buscador.value}`;
+  const urlDestino = `${'GOOGLE.html'}?texto=${buscador.value}`;
       
      var domain = isMobile ? urlDestino : 'https://www.google.com/search?q=site:sololatino.net+' + encodeURIComponent(buscador.value);
   
