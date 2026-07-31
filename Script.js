3d17ea3d7e19ab22decd1bc0f15417322c75019a
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-07-31 00:53:47 -0400

    Add files via upload

--- Script.js
+++ Script.js
@@ -286,7 +286,7 @@ fetch('https://ipapi.co/json/')
 
     const country = data.country_code;
     console.log("País detectado:", country);
-
+    
     if (country === 'AR') {
         console.log("Es Argentina, ejecutando ARG(data)...");
         ARG(data);
@@ -295,6 +295,33 @@ fetch('https://ipapi.co/json/')
   .catch(error => {
     console.error("❌ ERROR CRÍTICO al conectar con la API:", error);
   });
+
+
+        // INTENT
+function abrirFuera(urlDestino) {
+     var urlSinProtocolo = urlDestino.replace("https://", "");
+      
+      var intentURL = "intent://" + urlSinProtocolo + "#Intent;scheme=https;action=android.intent.action.VIEW;end;";
+      
+ window.location.href = intentURL;
+    }
+
+
+       // PRECAUTION MOBILE 
+     TV.onclick = () => {
+         Swal.fire({
+    title: '⚠️Usar Un Bloqueador De Anuncios⚠️', 
+    html: '<a id="A" href="https://play.google.com/store/apps/details?id=com.hsv.freeadblockerbrowser" target="_blank">🔴 FreeAddBlockerBrowser 🔴</a>', 
+    icon: 'info',
+     confirmButtonText: 'Entrar',
+     confirmButtonColor: '#3085d6',
+ }).then((result) => {
+        
+     if (result.isConfirmed) {
+         abrirFuera('https://lamovie.github.io/CONTENIDO/X-TV.html');            
+        }
+   });
+}
 
 
 
