dcc8a8af8b0948efabd51f0f3e81094a035f776a
LaMovie <156336185+LaMovie@users.noreply.github.com>
2026-09-02 12:47:19 -0400

    Add files via upload

--- Script2.js
+++ Script2.js
@@ -84,7 +84,9 @@ function procesarEnlace(matchedItem) {
     var CADENA = ['google.com/file', 'www.dropbox.com', 'play.vidyard', 'okpeliz.com', '.mp4'];
     
     if (CADENA.some(dominio => ENLACE.includes(dominio))) {
-        window.location.href = `PLAY.html?titulo=${encodeURIComponent(tituloLimpio)}&url=${encodeURIComponent(ENLACE)}`;
+        window.location.href = `PLAY.html?titulo=${encodeURIComponent(tituloLimpio)}&url=${ENLACE}`;
+   
+   
     } else {
         if (!isMobile && ENLACE.includes("latino.solo")) {
      var ENLACE = ENLACE.replace('latino.solo-latino', 'h5.swplayer');   
